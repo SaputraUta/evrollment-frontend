87f19df60684502dcce4d004ea0ae576b69d18de
Saputra <saputra.uta50@gmail.com>
2025-06-07 16:57:37 +0800

    Initial Commit

---
+++ Evrollment/EvrollmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvrollmentApp.swift
+//  Evrollment
+//
+//  Created by Saputra on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EvrollmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
